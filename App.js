81c55fc8bfa25a3edf598a57fc8ccd018cc6642c
Alexandre Esparrell Neves <alexandreesparrellneves01@gmail.com>
2023-06-14 09:08:00 -0300

    atualização do quiz

--- App.js
+++ App.js
@@ -6,11 +6,11 @@ const largura = Dimensions.get('window').width;
 
 export default function App() {
 
-    function botão2(){
+    function falso(){
         alert('c é burro?!')
     }
 
-    function botão1(){
+    function verdadeiro(){
       alert('é muito qi!')
   }
     
@@ -30,7 +30,7 @@ export default function App() {
        <Text style={styles.texto}>
             verdadeiro
        </Text>
-        <Button style={{color:'black'}} title='verdadeiro' color='#212A3E' accessibilitylabel='botão OK' onPress={botão1}></Button>
+        <Button style={{color:'black'}} title='verdadeiro' color='#212A3E' accessibilitylabel='botão OK' onPress={verdadeiro}></Button>
           <Text>{'\n'}[]
           
           </Text>
@@ -40,11 +40,135 @@ export default function App() {
       <Text style={styles.texto}>
         falso
       </Text>
-        <Button title='falso' color='#212A3E' accessibilitylabel='botão OK' onPress={botão2}></Button>
+        <Button title='falso' color='#212A3E' accessibilitylabel='botão OK' onPress={falso}></Button>
         <Text>{'\n'}</Text>
     </View>
     </View>
   </View>
+
+
+
+
+
+  <View style={{marginTop:'50px'}}>
+      <View>
+        <Image style={{width :largura/1.5, height:largura/1.5}} source={{uri:'https://www.macworld.com/wp-content/uploads/2023/03/apple_march_2022_event_confirmed.jpg?resize=1200%2C675&quality=50&strip=all'}}/>
+      </View>
+      <View style={{background:'black',borderRadius:'5px',width : largura/1.5}}>
+        <Text style={{textAlign: 'center',fontSize: 50,width :largura/1.5,color:'white'}}>iOS{'\n'}</Text>
+        <Text style={{color:'white',textAlign: 'center'}}>
+        o primeiro computador pessoal de Jobs foi o apple lisa</Text>
+        
+    <View>
+       <Text style={styles.texto}>
+            verdadeiro
+       </Text>
+        <Button style={{color:'black'}} title='verdadeiro' color='#212A3E' accessibilitylabel='botão OK' onPress={falso}></Button>
+          <Text>{'\n'}[]
+          
+          </Text>
+    </View>
+
+    <View>
+      <Text style={styles.texto}>
+        falso
+      </Text>
+        <Button title='falso' color='#212A3E' accessibilitylabel='botão OK' onPress={verdadeiro}></Button>
+        <Text>{'\n'}</Text>
+    </View>
+    </View>
+  </View>
+
+  <View style={{marginTop:'50px'}}>
+      <View>
+        <Image style={{width :largura/1.5, height:largura/1.5}} source={{uri:'https://www.macworld.com/wp-content/uploads/2023/03/apple_march_2022_event_confirmed.jpg?resize=1200%2C675&quality=50&strip=all'}}/>
+      </View>
+      <View style={{background:'black',borderRadius:'5px',width : largura/1.5}}>
+        <Text style={{textAlign: 'center',fontSize: 50,width :largura/1.5,color:'white'}}>iOS{'\n'}</Text>
+        <Text style={{color:'white',textAlign: 'center'}}>
+        o criador da Apple foi o Bill Gates?</Text>
+        
+    <View>
+       <Text style={styles.texto}>
+            verdadeiro
+       </Text>
+        <Button style={{color:'black'}} title='verdadeiro' color='#212A3E' accessibilitylabel='botão OK' onPress={falso}></Button>
+          <Text>{'\n'}[]
+          
+          </Text>
+    </View>
+
+    <View>
+      <Text style={styles.texto}>
+        falso
+      </Text>
+        <Button title='falso' color='#212A3E' accessibilitylabel='botão OK' onPress={verdadeiro}></Button>
+        <Text>{'\n'}</Text>
+    </View>
+    </View>
+  </View>
+
+
+  <View style={{marginTop:'50px'}}>
+      <View>
+        <Image style={{width :largura/1.5, height:largura/1.5}} source={{uri:'https://www.macworld.com/wp-content/uploads/2023/03/apple_march_2022_event_confirmed.jpg?resize=1200%2C675&quality=50&strip=all'}}/>
+      </View>
+      <View style={{background:'black',borderRadius:'5px',width : largura/1.5}}>
+        <Text style={{textAlign: 'center',fontSize: 50,width :largura/1.5,color:'white'}}>iOS{'\n'}</Text>
+        <Text style={{color:'white',textAlign: 'center'}}>
+        o o nome da loja é apple store</Text>
+        
+    <View>
+       <Text style={styles.texto}>
+            verdadeiro
+       </Text>
+        <Button style={{color:'black'}} title='verdadeiro' color='#212A3E' accessibilitylabel='botão OK' onPress={verdadeiro}></Button>
+          <Text>{'\n'}[]
+          
+          </Text>
+    </View>
+
+    <View>
+      <Text style={styles.texto}>
+        falso
+      </Text>
+        <Button title='falso' color='#212A3E' accessibilitylabel='botão OK' onPress={falso}></Button>
+        <Text>{'\n'}</Text>
+    </View>
+    </View>
+  </View>
+
+
+  <View style={{marginTop:'50px'}}>
+      <View>
+        <Image style={{width :largura/1.5, height:largura/1.5}} source={{uri:'https://www.macworld.com/wp-content/uploads/2023/03/apple_march_2022_event_confirmed.jpg?resize=1200%2C675&quality=50&strip=all'}}/>
+      </View>
+      <View style={{background:'black',borderRadius:'5px',width : largura/1.5}}>
+        <Text style={{textAlign: 'center',fontSize: 50,width :largura/1.5,color:'white'}}>iOS{'\n'}</Text>
+        <Text style={{color:'white',textAlign: 'center'}}>
+        oQuando o primeiro iphone foi lançado em 2007</Text>
+        
+    <View>
+       <Text style={styles.texto}>
+            verdadeiro
+       </Text>
+        <Button style={{color:'black'}} title='verdadeiro' color='#212A3E' accessibilitylabel='botão OK' onPress={verdadeiro}></Button>
+          <Text>{'\n'}[]
+          
+          </Text>
+    </View>
+
+    <View>
+      <Text style={styles.texto}>
+        falso
+      </Text>
+        <Button title='falso' color='#212A3E' accessibilitylabel='botão OK' onPress={falso}></Button>
+        <Text>{'\n'}</Text>
+    </View>
+    </View>
+  </View>
+
+
     </SafeAreaView>
   );
 }
